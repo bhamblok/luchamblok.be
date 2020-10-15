c93d4a032a8ed56286df490c759e36b8d6222e6f
Bob Hamblok <post@bhamblok.be>
2020-10-15 14:39:37 +0200

    update js

--- js/luchamblok.js
+++ js/luchamblok.js
@@ -1,23 +1,5 @@
-// polyfill
-if (window.Element && !Element.prototype.closest) {
-  Element.prototype.closest = 
-  function(s) {
-    var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-        i,
-        el = this;
-    do {
-      i = matches.length;
-      while (--i >= 0 && matches.item(i) !== el) {};
-    } while ((i < 0) && (el = el.parentElement)); 
-    return el;
-  };
-}
-
-var gridItems;
-
 document.addEventListener("DOMContentLoaded", function(e) {
-  gridItems = document.querySelectorAll('.grid-item img');
-  var scrollToY = 0;
+  var gridItems = document.querySelectorAll('.grid-item img');
   if (gridItems.length) {
     var loaded = 0;
     var msnry = null;
@@ -31,23 +13,13 @@ document.addEventListener("DOMContentLoaded", function(e) {
         }
       });
       gridItem.parentNode.addEventListener('click', function(e) {
-        scrollToY = window.scrollY;
-        // var bounds = e.target.getBoundingClientRect();
-        // var zoom = document.querySelector('#zoom');
-        // zoom.style.width = bounds.width+'px';
-        // zoom.style.height = bounds.height+'px';
-        // zoom.style.left = bounds.left+'px';
+        const scrollToY = window.scrollY;
         document.querySelector('#zoom .title').innerHTML = e.target.closest('.grid-item').querySelector('.title').innerHTML
         const zoom = document.querySelector('#zoom');
         const img = document.querySelector('#zoom img');
         zoom.replaceChild(gridItem.cloneNode(true), img);
-        // console.log(gridItem);
-        // document.querySelector('#zoom img').src = e.target.closest('.grid-item').querySelector('img').src;
         setTimeout(function() {
           function closeZoom(e) {
-            // if (document.body.classList.contains('zoom')) {
-            //   document.querySelector('#zoom').removeAttribute('style');
-            // }
             zoom.removeEventListener('click', closeZoom);
             document.body.classList.toggle('zoom');
             console.log(scrollToY);
